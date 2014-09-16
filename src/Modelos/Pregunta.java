913d92662d63cc7f8121630d79eaa77ead4b28e4
Miguel <eagerxe@users.noreply.github.com>
2014-09-16 13:21:32 -0500

    Vista inicio, crear examen y modelo pregunta
    160914a

---
+++ src/Modelos/Pregunta.java
@@ -0,0 +1,19 @@
+
+package Modelos;
+
+/**
+ *
+ * @author miguelhernandez
+ */
+public class Pregunta {
+    String tipo;
+    String pregunta;
+    String respuesta;
+    String respuestas[];
+    public Pregunta (){
+        tipo=null;
+        pregunta=null;
+        respuesta=null; 
+        respuestas=null;
+    }
+}
